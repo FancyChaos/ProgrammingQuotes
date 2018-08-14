323e7919b5bab942fcc99cdd5d70f06def7bff7b
Felix Scheja <Felix.Scheja@outlook.de>
2018-08-14 23:17:30 +0200

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/FancyChaos/programmingquotes',
-    version='1.0.0',
+    version='1.1.0',
     zip_safe=False,
 )
