cdc9c8ae45563b2afd73e3c11d05c0182b0160e6
FancyChaos <Felix.Scheja@outlook.de>
2018-08-15 22:59:04 +0200

    Added language support and more quotes

--- programmingquotes/cli.py
+++ programmingquotes/cli.py
@@ -7,9 +7,13 @@ from . import pq
 
 
 @click.command()
-def main(args=None):
-    """Console script for programmingquotes."""
-    pq.write_quote()
+@click.option("--language", "-l", default="both", help="Pick your language (DE/EN). Leave it empty for all languages")
+def main(language):
+    valid_langs = ["DE", "EN", "both"]
+    if any(x.lower() in language.lower() for x in valid_langs):
+        pq.write_quote(language)
+    else:
+        print("[!] specified language not supported")
     return 0
 
 
--- programmingquotes/pq.py
+++ programmingquotes/pq.py
@@ -10,23 +10,32 @@ except ImportError as e:
     import random
 
 
-def write_quote():
-    to_print = get_quote()
-    print(to_print["quote"])
-    print("-" + to_print["author"])
 
-
-def get_quote():
+def write_quote(lang):
+    # Get path of quotes.json
     path = os.path.abspath(os.path.dirname(__file__))
     json_path = os.path.join(path, "quotes.json")
 
+    # Open quotes.json
     json_decode = []
     with open(json_path) as json_file:
         json_decode = json.load(json_file)
-
+    
+    # Sort quotes by language
+    sorted_quotes = [x for x in json_decode if x["lang"].lower() == lang.lower() or lang == "both"]
+   
+    # Get quote with imported module
     if "secrets" in sys.modules:
-        to_print = secrets.choice(json_decode)
+        to_print = secrets.choice(sorted_quotes)
     elif "random" in sys.modules:
-        to_print = random.choice(json_decode)
-
-    return to_print
+        to_print = random.choice(sorted_quotes)
+    
+    # If not author is specified, the author will be chosen correspondingly to the language
+    if not to_print["author"]:
+        author = "Unbekannt" if lang.lower() == "de" else "Unknown"
+    else:
+        author = to_print["author"]
+    
+    # Print the actual quote
+    print(to_print["quote"])
+    print("-" + author)
